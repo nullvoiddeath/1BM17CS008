89593dc019f635aaf0d0bcbea36299e6be73fcd2
bmsce <nullvoiddeath@gmail.com>
2019-08-16 09:57:21 +0530

    16/08/2019

--- fib.py
+++ fib.py
@@ -1,10 +1,13 @@
+def fib(n):
+	t1 = 0
+	t2 = 1
+	nT = 1
+	for i in range(a):
+		print(nT)
+		nT = t1 +t2
+		t1 = t2
+		t2 = nT
+	
 a = int(input("Enter the number of fibonacci numbers: "))
-t1 = 0
-t2 = 1
-nT = 1
-for i in range(a):
-	print(nT)
-	nT = t1 +t2
-	t1 = t2
-	t2 = nT
+fib(a)
 
